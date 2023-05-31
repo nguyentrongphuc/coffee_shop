484093d0d9f1f97b51b1bca2a226e7e20bb769e6
Phuc Nguyen <mr.nguyentrongphuc@gmail.com>
2023-05-30 17:50:04 -0700

    Update postman

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -8,8 +8,7 @@ export const environment = {
   auth0: {
     url: 'phucnguyen.us', // the auth0 domain prefix
     audience: 'phuc', // the audience set for the auth0 app
-    //clientId: 'J804TumgtEPJ9Sr0MY6opWIu3SmgROM9', // the client id generated for the auth0 app
-    clientId: 'yGPLAtm4M7Rwbftw6vmcVQfCW7tJRZEm',
+    clientId: 'J804TumgtEPJ9Sr0MY6opWIu3SmgROM9', // the client id generated for the auth0 app
     callbackURL: 'http://localhost:8100', // the base url of the running ionic application. 
   }
 };
